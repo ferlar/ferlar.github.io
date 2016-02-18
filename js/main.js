6a702cf0217d59933a2eb89571b9ab10de22293e
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2016-02-18 21:03:02 +0000

    Refactor html, added mailto number links, updated hero

--- js/main.js
+++ js/main.js
@@ -2,14 +2,6 @@ function openChat(){
    $crisp.chat.open()
 };
 
-$( "#call-to-action" ).hover(
-  function() {
-    $( this ).addClass( "animated pulse infinite" );
-  }, function() {
-    $( this ).removeClass( "animated pulse infinite" );
-  }
-);
-
 function scrollDown(){
   $("html, body").animate({ scrollTop: $(document).height() }, 950);
 };
@@ -17,12 +9,4 @@ function scrollDown(){
 function scrollDownMobile(){
 	$('.button-collapse').sideNav('hide');
 	$("html, body").animate({ scrollTop: $(document).height() }, 950);
-}
-
-if (screen.width<= 750) {
-	$( window ).scroll(function() {
-  		 $('#icon1').addClass('animated bounceInLeft');
-		 $('#icon2').addClass('animated bounceInLeft');
-		 $('#icon3').addClass('animated bounceInLeft');
-	});
 }
