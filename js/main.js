d92c8e93d992acb3810afe8ae28388e45da81f8a
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2016-10-06 20:39:22 +0100

    Added scrollfire and animations

--- js/main.js
+++ js/main.js
@@ -9,4 +9,39 @@ function scrollDown(){
 function scrollDownMobile(){
 	$('.button-collapse').sideNav('hide');
 	$("html, body").animate({ scrollTop: $(document).height() }, 950);
-}
+};
+
+
+function moveShirt(){
+  $("#icon1").addClass("animated zoomIn");
+};
+
+function moveBuilding(){
+  $("#icon2").addClass("animated flipInX");
+};
+
+function moveCarrinha(){
+  $("#icon3").addClass("animated lightSpeedIn");
+};
+
+var options = [
+  {
+    selector: '#icon1',
+    offset: 50,
+    callback:
+     moveShirt()
+   },
+   {
+     selector: '#icon2',
+     offset: 50,
+     callback:
+      moveBuilding()
+    },
+    {
+    selector: '#icon3',
+    offset: 70,
+    callback:
+     moveCarrinha()
+   },
+];
+Materialize.scrollFire(options);
